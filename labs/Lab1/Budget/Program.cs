76ea6790f9ad1e06ad44660e5e22ced0f9d6ef71
Kaleb Dreier <kdreier@live.com>
2021-01-27 19:06:29 -0600

    Initial Creation

---
+++ labs/Lab1/Budget/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Budget
+ * ITSE 1430
+ * Spring 2021
+ * Kaleb Dreier
+ */
+
+using System;
+
+namespace Budget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
