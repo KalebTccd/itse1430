c323fefcb877ef5753e49dad2e79a7f38168b027
Kaleb Dreier <kdreier@live.com>
2021-02-05 20:48:22 -0600

    Story 3 Completed

--- labs/Lab1/Budget/Program.cs
+++ labs/Lab1/Budget/Program.cs
@@ -18,6 +18,10 @@ namespace Budget
         {
             bool done = false;
             Console.WriteLine("Budget\r\nITSE 1430\r\nSpring 2021\r\nKaleb Dreier\r\n\r\n");
+            SetUpAccount();
+            Console.WriteLine(accountName);
+            Console.WriteLine(accountNumber);
+            Console.WriteLine(startingBalance);
             do
             {
 
@@ -26,19 +30,54 @@ namespace Budget
         static void SetUpAccount()
         {
             string temp;
+            Console.WriteLine("Enter a name: ");
             accountName = Console.ReadLine();
             do
             {
+                Console.WriteLine("Please enter an account number: ");
                 temp = Console.ReadLine();
-            } while (temp.Length != 12 && temp.EndsWith('0') && temp.StartsWith('0') && temp.);
+            } while (!CheckAccountNumber(temp));
+            accountNumber = temp;
+            Console.WriteLine("Please enter a starting amount: ");
+            startingBalance = ReadDecimal(0);
         }
-        static bool NumberCheck(string value)
+        static bool CheckAccountNumber(string value)
         {
-            foreach (char c in value)
+            if (value.Length != 12)
             {
-                if (!char.IsDigit(c))
-                    return false;
+                DisplayError("Account Number is not long enough");
+                return false;
+            } else if (value.EndsWith('0'))
+            {
+                DisplayError("Account Number cannot have 0 at the end");
+                return false;
+            } 
+            else if (value.StartsWith('0'))
+            {
+                DisplayError("Account Number cannot have 0 at the beginning");
+                return false;
+            } 
+            else if (!Int64.TryParse(value, out _))
+            {
+                DisplayError("Account Number can only have numbers");
+                return false;
             }
+            return true;
+        }
+        static decimal ReadDecimal ( decimal minimumValue )
+        {
+            do
+            {
+                string input = Console.ReadLine();
+                if (Decimal.TryParse(input, out decimal result))
+                {
+                    if (result >= minimumValue)
+                        return result;
+                    else
+                        DisplayError("Value must be at least " + minimumValue);
+                }
+                DisplayError("Value must be numeric");
+            } while (true);
         }
         static void DisplayError ( string message )
         {
