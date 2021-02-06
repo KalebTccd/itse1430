00517cd2285c0e693f3a808a21ca1d1a4f61fb62
Kaleb Dreier <kdreier@live.com>
2021-02-05 19:06:38 -0600

    removed string args

--- labs/Lab1/Budget/Program.cs
+++ labs/Lab1/Budget/Program.cs
@@ -11,7 +11,7 @@ namespace Budget
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Budget\r\nITSE 1430\r\nSpring 2021\r\nKaleb Dreier\r\n\r\n");
         }
