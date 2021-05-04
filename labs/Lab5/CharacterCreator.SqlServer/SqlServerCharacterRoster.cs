be96cb7914b9af3dc8e275df123fdab759bbaae4
Kaleb Dreier <kdreier@live.com>
2021-05-04 18:46:27 -0500

    story 4 done

---
+++ labs/Lab5/CharacterCreator.SqlServer/SqlServerCharacterRoster.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace CharacterCreator.SqlServer
+{
+    /// <summary>Provides an implementation of <see cref="ICharacterRoster"/> using SQL Server.</summary>
+    /// <remarks>
+    /// Relies on ADO.NET for database access.
+    /// </remarks>
+    public class SqlServerCharacterRoster : ICharacterRoster
+    {
+        public SqlServerCharacterRoster ( string connectionString )
+        {
+            _connectionString = connectionString;
+        }
+        public Character Add ( Character character )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete ( int id )
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Character> GetAll ()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update ( int id, Character character )
+        {
+            throw new NotImplementedException();
+        }
+        private readonly string _connectionString;
+    }
+}
